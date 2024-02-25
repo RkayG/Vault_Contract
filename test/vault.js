85cdeef1118128f6a7f8ffcc19883cd88f32edd5
RkayG <111110427+RkayG@users.noreply.github.com>
2024-02-25 20:10:46 +0100

    Update vault.js
    uncommented some lines

--- test/vault.js
+++ test/vault.js
@@ -28,13 +28,14 @@ describe('Vault', function () {
     });
 
     it('should deposit funds with correct grant information', async () => {
-        const unlockDurationMins = 1; // Unlock duration in minutes
+        const unlockDurationMins = 5; // Unlock duration in minutes
 
-        // Calculate the unlock time (1 minute from the current timestamp)
+        // Calculate the unlock time (5 minutes from the current timestamp)
         const unlockTime = Math.floor(Date.now() / 1000) + unlockDurationMins * 60;
         
         const amount = ethers.parseEther("1");
-        // Deposit 1 ether with a release time of 1 minute
+        
+        // Deposit 1 ether with a release time of 5 minutes
         await vault.deposit(beneficiary.address, unlockDurationMins, { from: donor.address, value: amount } ) ;
 
 
@@ -45,22 +46,22 @@ describe('Vault', function () {
         assert.equal(grantInfo.donor, donor.address, "Donor should be the caller");
         assert.equal(grantInfo.beneficiary, beneficiary.address, "Beneficiary address should match");
         assert.equal(grantInfo.amount, ethers.parseEther("1"), "Amount should be 1 ether");
-        //assert.equal(grantInfo.unlockTime.toString(), unlockTime, "Unlock time should be 1 minute from now");
+        assert.equal(grantInfo.unlockTime.toString(), unlockTime, "Unlock time should be 5 minutes from now");
         assert.equal(grantInfo.claimed, false, "Claimed status should be false");
     });
 
     it("should allow beneficiary to claim funds after release time", async () => {
-        const unlockDurationMins = 2; // Unlock duration in minutes
+        const unlockDurationMins = 6 // Unlock duration in minutes
 
-        // Calculate the unlock time (3 minutes from the current timestamp)
-      /*   const unlockTime = Math.floor(Date.now() / 1000) + unlockDurationMins * 60;
+        // Calculate the unlock time (6 minutes from the current timestamp)
+        const unlockTime = Math.floor(Date.now() / 1000) + unlockDurationMins * 60;
 
         // Calculate the duration to wait in milliseconds
         const delayMilliseconds = (unlockTime - Math.floor(Date.now() / 1000)) * 1000;
 
         // Create a Promise with setTimeout to wait until the unlock time
         await new Promise(resolve => setTimeout(resolve, delayMilliseconds)); 
- */
+ 
        // Call the claim function with the beneficiary address
         await vault.connect(beneficiary).claim();
        
